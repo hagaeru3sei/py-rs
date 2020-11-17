b868153339245beb71854aa71999cb34321c8c2e
Nobuaki Mochizuki <hagaeru3sei@gmail.com>
2020-11-17 18:06:10 +0900

    Add docker file

--- hello.py
+++ hello.py
@@ -1,17 +1,45 @@
+#!/usr/bin/env python3
 import sys
 import os
+import pprint
+import typing
+import re
+import itertools
+import time
+from concurrent.futures import ThreadPoolExecutor
+print("VERSION INFO:", sys.version_info)
+print("MODULES:", sys.modules.keys())
+print("PYTHONPATH:", os.environ.get('PYTHONPATH'))
+print("PATH:", os.environ.get('PATH'))
+print("")
+try:
+    import joblib
+except ImportError as e:
+    print("")
+    print("ImportError!", "(joblib)", "-"*128)
+    print(e)
+
 try:
     import numpy as np
+    print(f'NUMPY: {np}')
 except ImportError as e:
     np = None
+    print("")
+    print("ImportError!", "(Numpy)", "-"*128)
     print(e)
 
 def say(message="world"):
-    print(f'numpy: {np}')
     max_key = max(dict(os.environ).keys())
     l = len(max_key)
     for k, v in os.environ.items():
         print(f'{k:{l}}: {v}')
+    
+    if np:
+        a = np.random.rand(10000, 10000)
+        s = time.time()
+        _ = np.linalg.inv(a)
+        print("exec time:", time.time() - s)
+
     return f'Hello {message}'
 
 #if __name__ == "__main__":
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,28 @@
-use numpy::PyArray1;
-//use pyo3::prelude::{PyResult, Python};
-use pyo3::types::IntoPyDict;
-use pyo3::types::PyDict;
-use pyo3::types::PyList;
-use pyo3::prelude::*;
+use pyo3::{
+    types::{
+        PyBytes, PyDict, PyList, IntoPyDict
+    }, 
+    prelude::*
+};
 use std::env;
 
-fn main() -> PyResult<()> {
-    // v 0.12
-    //Python::with_gil(|py| {
-    //    let np = py.import("numpy")?;
-    //    let locals = [("np", np)].into_py_dict(py);
-    //    let pyarray: &PyArray1<i32> = py
-    //        .eval("np.absolute(np.array([-1, -2, -3], dtype='int32'))", Some(locals), None)?
-    //        .extract()?;
-    //    let readonly = pyarray.readonly();
-    //    let slice = readonly.as_slice()?;
-    //    assert_eq!(slice, &[1, 2, 3]);
-    //    Ok(())
-    //})
+//fn main() -> PyResult<()> {
+//    // v 0.12
+//    Python::with_gil(|py| {
+//        let np = py.import("numpy")?;
+//        let locals = [("np", np)].into_py_dict(py);
+//        let pyarray: &PyArray1<i32> = py
+//            .eval("np.absolute(np.array([-1, -2, -3], dtype='int32'))", Some(locals), None)?
+//            .extract()?;
+//        let readonly = pyarray.readonly();
+//        let slice = readonly.as_slice()?;
+//        assert_eq!(slice, &[1, 2, 3]);
+//        Ok(())
+//    })
+//    Ok(())
+//}
 
+fn main() -> PyResult<()> {
     let gil = Python::acquire_gil();
     let py = gil.python();
     let path: &PyList = py.import("sys").unwrap().get("path").unwrap().try_into().unwrap();
@@ -33,5 +36,40 @@ fn main() -> PyResult<()> {
     println!("{}", res);
     //let _np = py.import("numpy")?;
     //println!("{}", _np);
+
+    let locals = PyDict::new(py);
+    py.run(r#"
+try:
+    import base64
+    s = 'Hello Rust!'
+    ret = base64.b64encode(s.encode('utf-8'))
+except ImportError as e:
+    ret = b"SGVsbG8gUnVzdCE="
+    print(e)
+"#, None, Some(locals)).unwrap();
+    let ret = locals.get_item("ret").unwrap();
+    let b64: &PyBytes = ret.downcast().unwrap();
+    assert_eq!(b64.as_bytes(), b"SGVsbG8gUnVzdCE=");
+
     Ok(())
 }
+
+//fn main_(py: Python) -> PyResult<()> {
+//    let sys = py.import("sys")?;
+//    let version: String = sys.get("version")?.extract()?;
+//    let locals = [("os", py.import("os")?)].into_py_dict(py);
+//    let code = "os.getenv('USER') or os.getenv('USERNAME') or 'Unknown'";
+//    let user: String = py.eval(code, None, Some(&locals))?.extract()?;
+//    println!("Hello {}, I'm Python {}", user, version);
+//    Ok(())
+//}
+//
+//fn main() -> Result<(), ()> {
+//    Python::with_gil(|py| {
+//        main_(py).map_err(|e| {
+//          // We can't display Python exceptions via std::fmt::Display,
+//          // so print the error here manually.
+//          e.print_and_set_sys_last_vars(py);
+//        })
+//    })
+//}
